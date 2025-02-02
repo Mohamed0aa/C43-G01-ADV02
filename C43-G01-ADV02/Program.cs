1a51524243c71251370c653eca26c66456d3e471
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-02-03 00:02:36 +0200

    Add project files.

---
+++ C43-G01-ADV02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C43_G01_ADV02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
